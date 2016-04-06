55be054537fdac12976262287006843a8da3e35f
张铁君 <zhangtj@zhangtiejundeMacBook-Pro.local>
2016-04-06 12:32:48 +0800

    Initial Commit

---
+++ LivingCircle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LivingCircle
+//
+//  Created by 张铁君 on 16/4/6.
+//  Copyright (c) 2016年 沈阳云融. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ LivingCircleTests/LivingCircleTests.m
@@ -0,0 +1,34 @@
+//
+//  LivingCircleTests.m
+//  LivingCircleTests
+//
+//  Created by 张铁君 on 16/4/6.
+//  Copyright (c) 2016年 沈阳云融. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LivingCircleTests : XCTestCase
+
+@end
+
+@implementation LivingCircleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
